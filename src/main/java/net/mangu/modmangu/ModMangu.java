85be3bc9ae190a670164a190c120e0ba52d16378
mangu <nicularazvan10@gmail.com>
2023-01-02 12:14:26 +0200

    added important comment

--- src/main/java/net/mangu/modmangu/ModMangu.java
+++ src/main/java/net/mangu/modmangu/ModMangu.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//very important comment
 public class ModMangu implements ModInitializer {
 	public static final String MOD_ID = "modmangu";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
